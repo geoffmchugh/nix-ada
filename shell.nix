31e3f2a1dc7350fc85525ee6b9c59fa7ce974258
Geoff McHugh <geoff.mchugh@gmail.com>
2026-03-01 19:54:34 +0000

    add gnatcoll-core, reformat shell.nix

--- shell.nix
+++ shell.nix
@@ -2,7 +2,18 @@
   nix-ada ? import ../nix-ada { pkgs = pkgs; }
 }:
 pkgs.mkShell {
-  nativeBuildInputs = [ pkgs.gnat pkgs.alire nix-ada.alire-index nix-ada.gtkada pkgs.gprbuild nix-ada.aws nix-ada.gnat_util nix-ada.gnat-gdb-scripts nix-ada.gnatstudio ];
+  nativeBuildInputs = [ 
+    pkgs.gnat 
+    pkgs.alire 
+    nix-ada.alire-index 
+    nix-ada.gtkada
+    pkgs.gprbuild 
+    pkgs.gnatcoll-core 
+    nix-ada.aws 
+    nix-ada.gnat_util 
+    nix-ada.gnat-gdb-scripts 
+    nix-ada.gnatstudio 
+  ];
 #   nativeBuildInputs = [ nix-ada.gtkada nix-ada.pkgs.gprbuild nix-ada.pkgs.gnatPackages.gpr2 nix-ada.pkgs.gnat nix-ada.pkgs.alire nix-ada.gnatstudio ];
 #   nativeBuildInputs =[ nix-ada.gnatcoverage ];
 #   nativeBuildInputs = [ nix-ada.wayland-ada-scanner ];
